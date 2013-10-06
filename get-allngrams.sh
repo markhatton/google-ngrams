274a7409a2a15ac980010c5e06c7d739a5529ca1
Mark Hatton <mark@zeebox.com>
2013-10-06 14:24:49 +0100

    support for unigrams

--- get-allngrams.sh
+++ get-allngrams.sh
@@ -6,11 +6,15 @@ USAGE="USAGE: $SCRIPT n [a-z], e.g.: $SCRIPT 3 \"a b c\""
 AZ="a b c d e f g h i j k l m n o p q r s t u v w x y z"
 
 n=${1?$USAGE}
-alpha=${2-$AZ}
+c1s=${2-$AZ}
+c2s="_ $AZ"
 
-for c1 in $alpha; do
-for c2 in _ $AZ; do
+[[ $n -eq 1 ]] && c2s="n/a"
+
+for c1 in $c1s; do
+for c2 in $c2s; do
     prefix="$c1$c2"
+    [[ $n -eq 1 ]] && prefix=$c1
 
     echo "Fetching $prefix..."
     $(dirname $0)/get-ngrams.sh $n $prefix
