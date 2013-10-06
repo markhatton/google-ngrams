3015bd916493cdb8a6ec6443a88cc1f6c1284c53
Mark Hatton <mark@zeebox.com>
2013-10-06 13:50:25 +0100

    initial commit

---
+++ get-allngrams.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SCRIPT=$(basename $0)
+USAGE="USAGE: $SCRIPT n [a-z], e.g.: $SCRIPT 3 \"a b c\""
+
+AZ="a b c d e f g h i j k l m n o p q r s t u v w x y z"
+
+n=${1?$USAGE}
+alpha=${2-$AZ}
+
+for c1 in $alpha; do
+for c2 in _ $AZ; do
+    prefix="$c1$c2"
+
+    echo "Fetching $prefix..."
+    $(dirname $0)/get-ngrams.sh $n $prefix
+done
+done
---
+++ get-ngrams.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SCRIPT=$(basename $0)
+USAGE="USAGE: $SCRIPT n prefix, e.g.: $SCRIPT 3 aa"
+
+n=${1?$USAGE}
+prefix=${2?$USAGE}
+
+FILTER_PATTERN="[a-zA-Z' ]\+.[0-9][0-9][0-9][0-9].[0-9]\+.[0-9]\+"
+
+BASEURL="http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all"
+PUBDATE="20120701"
+
+curl -s "$BASEURL-${n}gram-$PUBDATE-${prefix}.gz" | \
+    gunzip | \
+    LC_ALL=C grep -x "$FILTER_PATTERN" | \
+    awk -F'\t' '{ if ($1 == last) { count+=$3 } else if (last != "") { print last"\t"count; count=$3 }; last=$1 }' | \
+    LC_ALL=C sort | \
+    bzip2 > ${n}gram-${prefix}.csv.bz2
+
